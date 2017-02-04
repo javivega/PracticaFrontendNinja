763922a2f09fcbb97026c666f79416d1e7a69826
miguel angel Arribas Sanchez <miguel angel Arribas Sanchez>
2017-02-04 13:38:40 +0100

    sigo con detalles

--- src/js/ready.js
+++ src/js/ready.js
@@ -2,26 +2,22 @@ var $ = require('jquery');
 var commentsManager = require('./commentsManager');
 require('./noframework.waypoints.min');
 
+
 $(document).ready(function () {
     
     commentsManager.loadComments();
-
     //Muestro y oculto comentarios en el scroll
+
     var waypoint = new Waypoint({
         element: document.getElementById('js--comments-section')
         , handler: function (direction) {
-            if(direction == "down"){
-
-              this.element.classList.add("comment-shown");
-            } else {
-                this.element.classList.remove("comment-shown");
-
+            if (direction == "down") {
+                this.element.classList.add("comment-shown");
             }
-
+            else {
+                this.element.classList.remove("comment-shown");
+            }
         }
         , offset: 800
     })
-
-
-
 });
